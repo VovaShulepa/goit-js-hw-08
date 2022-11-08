5cc84bf77ae4478c972e145a2e9a7ba2328b32cd
Shulepa Volodymyr <shulepatrader@gmail.com>
2022-11-08 20:55:11 +0200

    add vimeo

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,33 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryEl = document.querySelector('.gallery');
+
+galleryEl.insertAdjacentHTML('beforeend', createGalleryItems(galleryItems));
+
+function createGalleryItems(items) {
+  return items
+    .map(
+      item => `
+        <a class="gallery__item" href="${item.original}">
+         <img
+         class="gallery__image"
+         src="${item.preview}"
+         alt="${item.description}"
+        />
+        </a>
+     `
+    )
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
 console.log(galleryItems);
