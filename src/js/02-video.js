14b3447f877db33125280a7401f401ce4bf1e024
Shulepa Volodymyr <shulepatrader@gmail.com>
2022-11-08 22:25:34 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash/throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const throttle = require('lodash.throttle');
+
+player.on('timeupdate', onPlay);
+
+const onPlay = function (data) {
+  // data is an object containing properties specific to that event
+};
+
+player.setCurrentTime();
